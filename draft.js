1e9b21e3901cfd194d5b1ef5c0a043ae5d63b016
Bent Cardan <bent@nothingsatisfies.com>
2014-05-19 00:12:11 -0400

    sp

--- draft.js
+++ draft.js
@@ -7,13 +7,13 @@ function consolidateMsg(message){
     console.log(message)
 }
 
-function resurectChildren(){
+function resurrectChildren(){
   var p1 = require('child_process').fork('./p1');
   p1.on('message',function(m){consolidateMsg(m)});
   p1.on('exit',function(){
-    resurectChildren();
+    resurrectChildren();
   })
 }
 
 
-resurectChildren();
+resurrectChildren();
